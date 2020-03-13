5c742a5778f111cbf983fbf31c3b27309d344ca3
gabriellebyers <gabriellembyers@gmail.com>
2020-03-12 20:49:10 -0500

    added firebase config

---
+++ comicbook.js
@@ -0,0 +1,20 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.10.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAD8D_DS1TRkJXkWT4g_Y57Itjw_oIcpwU",
+    authDomain: "comic-book-search.firebaseapp.com",
+    databaseURL: "https://comic-book-search.firebaseio.com",
+    projectId: "comic-book-search",
+    storageBucket: "comic-book-search.appspot.com",
+    messagingSenderId: "167980205893",
+    appId: "1:167980205893:web:1df86c3da4a161ef7da02c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
